388255ed11d87795acd52ee7eeddb905d9c1f399
Michel Medeiros <michel.medeiros@hotmart.com>
2019-07-30 09:23:35 -0300

    Remove imports

--- src/test/java/com/hotmart/cucumber/poc/e2e/defs/GivenSteps.java
+++ src/test/java/com/hotmart/cucumber/poc/e2e/defs/GivenSteps.java
@@ -1,11 +1,8 @@
 package com.hotmart.cucumber.poc.e2e.defs;
 
 import com.hotmart.cucumber.poc.e2e.E2ETestIntegration;
-import com.hotmart.cucumber.poc.e2e.commons.Hooks;
-import com.hotmart.cucumber.poc.e2e.commons.ResultMap;
 import cucumber.api.java.en.Given;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
 public class GivenSteps extends E2ETestIntegration {
--- src/test/java/com/hotmart/cucumber/poc/e2e/defs/ThenSteps.java
+++ src/test/java/com/hotmart/cucumber/poc/e2e/defs/ThenSteps.java
@@ -4,11 +4,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import com.hotmart.cucumber.poc.e2e.E2ETestIntegration;
-import com.hotmart.cucumber.poc.e2e.commons.Hooks;
-import com.hotmart.cucumber.poc.e2e.commons.ResultMap;
 import cucumber.api.java.en.Then;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
 public class ThenSteps extends E2ETestIntegration {
